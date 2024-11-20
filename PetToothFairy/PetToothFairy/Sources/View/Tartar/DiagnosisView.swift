93a27a3f7ddec760bce9642f117a99b841ee0569
임주민 <limj1213@gmail.com>
2024-11-21 07:12:11 +0900

    feat - #3 치석 진단을 위한 뷰 구현

--- PetToothFairy/PetToothFairy/Sources/View/Tartar/DiagnosisView.swift
+++ PetToothFairy/PetToothFairy/Sources/View/Tartar/DiagnosisView.swift
@@ -8,59 +8,106 @@
 import SwiftUI
 
 struct DiagnosisView: View {
-    var body: some View {
-        ZStack{
-            VStack {
-                Text("치석 판단")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 28)
-                    .padding(.top, 16)
-                
-                ZStack{
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(height: 380)
-                        .cornerRadius(10)
-                        .padding(.leading, 16)
-                        .padding(.trailing, 16)
-                    
-                    VStack(spacing: 22){
-                        Group{
-                            Text("1개")
-                                .foregroundColor(Color(hex: "3561E6"))
-                                .font(.system(size: 16))
-                                .fontWeight(.semibold) +
-                            Text("의 치석이 있는 것으로 추정돼요.")
-                                .foregroundColor(.black)
-                                .font(.system(size: 16))
-                                .fontWeight(.semibold)
-                        }
-                        .padding(.top, 46)
-    
-                        Image("image_tartar")
-                            .frame(width: 284, height: 181)
-                            
-                        
-                        Text("해당 치석 AI 판단 서비스는 전문의의 진료를 대체하지 않습니다. \n이 점을 고려하여 서비스를 이용해주시기 바랍니다.")
-                            .font(.system(size: 10))
-                            .padding(.top, 22)
-                        Spacer()
-                    }.frame(height: 380)
-                }
-                Spacer()
-            }
-            .background(Color(hex: "F5F5F5"))
-       
-        }
-        .navigationBarTitle("", displayMode: .inline)
-        .navigationBarHidden(true)
+  let result: [DiagnosisModel]
+  let imageSize: ImageDetails
+  let count: Int
+  @Binding var pickedImage: UIImage
+  
+  var body: some View {
+    ZStack {
+      VStack {
+        Text("치석 판단")
+          .font(.system(size: 24))
+          .fontWeight(.bold)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(.leading, 28)
+          .padding(.top, 16)
+        
+        mainContent
+        Spacer()
+      }
+      .background(Color(hex: "F5F5F5"))
     }
+    .navigationBarTitle("", displayMode: .inline)
+    .navigationBarHidden(true)
+  }
+  
+  // 메인 콘텐츠를 위한 서브 뷰
+  private var mainContent: some View {
+    ZStack {
+      Rectangle()
+        .fill(Color.white)
+        .frame(height: 380)
+        .cornerRadius(10)
+        .padding(.leading, 16)
+        .padding(.trailing, 16)
+      
+      VStack(spacing: 22) {
+        diagnosisText
+        imageWithAnnotations
+        disclaimerText
+        petHospitalMapView
+        Spacer()
+      }
+      .frame(height: 380)
+    }
+  }
+  
+  // 진단 텍스트를 위한 서브 뷰
+  private var diagnosisText: some View {
+    Group {
+      Text("\(count)개")
+        .foregroundColor(Color(hex: "3561E6"))
+        .font(.system(size: 23))
+        .fontWeight(.semibold) +
+      Text("의 치석이 있는 것으로 추정돼요.")
+        .foregroundColor(.black)
+        .font(.system(size: 18))
+        .fontWeight(.semibold)
+    }
+    .padding(.top, 46)
+  }
+  
+  // 이미지와 강조 표시된 치석 위치를 위한 서브 뷰
+  private var imageWithAnnotations: some View {
+    GeometryReader { geometry in
+        ZStack {
+            // 이미지를 중앙에 배치
+            Image(uiImage: pickedImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: imageSize.cgWidth, height: imageSize.cgHeight)
+                .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+          
+            // 결과 배열을 기준으로 사각형 그리기
+            ForEach(result, id: \.self) { diagnosis in
+                // 이미지 왼쪽 위를 기준으로 x, y 위치 계산
+                let scaledX = CGFloat(diagnosis.position.x)
+                let scaledY = CGFloat(diagnosis.position.y)
+
+                Rectangle()
+                    .stroke(Color.yellow, lineWidth: 2)
+                    .frame(width: diagnosis.size.width, height: diagnosis.size.height)
+                    .position(
+                      x: (geometry.size.width / 2) + (scaledX - (imageSize.cgWidth / 2)), // 이미지 중앙을 기준으로 x 조정
+                      y: (geometry.size.height / 2) + (scaledY - (imageSize.cgHeight / 2))  // 이미지 중앙을 기준으로 y 조정
+                    )
+            }
+        }
+        .frame(width: geometry.size.width, height: geometry.size.height)
+    }
+  }
+  
+  // 서비스 사용에 대한 안내 문구를 위한 서브 뷰
+  private var disclaimerText: some View {
+    Text("해당 치석 AI 판단 서비스는 전문의의 진료를 대체하지 않습니다. \n이 점을 고려하여 서비스를 이용해주시기 바랍니다.")
+      .font(.system(size: 12))
+      .scaledToFit()
+      .padding(.top, 22)
+  }
 }
 
-struct DiagnosisView_Previews: PreviewProvider {
-    static var previews: some View {
-        DiagnosisView()
-    }
+private var petHospitalMapView: some View {
+  PetHospitalMapView()
+    .frame()
 }
--- PetToothFairy/PetToothFairy/Sources/View/Tartar/UploadImageView.swift
+++ PetToothFairy/PetToothFairy/Sources/View/Tartar/UploadImageView.swift
@@ -8,98 +8,130 @@
 import SwiftUI
 
 struct UploadImageView: View {
-    var body: some View {
-        ZStack{
-            VStack(spacing: 16){
-                HStack{
-                    Text("치석 판단")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 28)
-                        .padding(.top, 16)
-                }
-                
-                ZStack{
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(height:84)
-                        .cornerRadius(10)
-                        .padding(.leading, 16)
-                        .padding(.trailing, 16)
-                    HStack{
-                        Image("icon_magnifyingGlass")
-                            .padding(.leading, 36)
-                        VStack(spacing: 4){
-                            Text("망고에게 치석이 있는 것 같나요?")
-                                .font(.system(size: 10))
-                                .foregroundColor(Color(hex: "50505E"))
-                                .padding(.leading, 6)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text("치석이 의심되는 부분을 AI로 판단할 수 있어요")
-                                .font(.system(size: 12))
-                                .foregroundColor(Color(hex: "0062FF"))
-                                .padding(.leading, 6)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        Spacer()
-                    }
-                }
-                
-                ZStack{
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(height: 320)
-                        .cornerRadius(10)
-                        .padding(.leading, 16)
-                        .padding(.trailing, 16)
-                        .cornerRadius(10)
-                    
-                    VStack{
-                        Text("주의 사항")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 16))
-                            .padding(.top, 24)
-                        Text("반려견의 입술을 들어올려 치아가 잘 보이도록 찍어주세요!")
-                            .font(.system(size: 12))
-                            .padding(.top, 10)
-                        HStack(spacing: 22){
-                            VStack(spacing: 6){
-                                Image("image_tartar2")
-                                Text("예시 사진")
-                                    .font(.system(size: 10))
-                                    .foregroundColor(Color(hex: "50505E"))
-                            }
-                            VStack(spacing: 6){
-                                Image("image_tartar3")
-                                Text("예시 사진")
-                                    .font(.system(size: 10))
-                                    .foregroundColor(Color(hex: "50505E"))
-                            }
-                        }.padding(.top, 24)
-                        
-                        NavigationLink(destination: DiagnosisView()) {
-                            Image("button_uploadImage")
-                                .padding(.top, 28)
-                        }
-                        Spacer()
-                        
-                    }.frame(height: 320)
-                    
-                }
-                
-                Spacer()
-                
-            }
-            .background(Color(hex: "F5F5F5"))
+  @StateObject private var tartarAnalysisVM = TartarAnalysisViewModel()
+  @State private var showingDiagnosisView = false
+  @State private var pickedImage: UIImage = UIImage(imageLiteralResourceName: "image_noimage")
+  
+  var body: some View {
+    ZStack{
+      VStack(spacing: 16){
+        HStack{
+          Text("치석 판단")
+            .font(.system(size: 24))
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 28)
+            .padding(.top, 16)
         }
-        .navigationBarTitle("", displayMode: .inline)
-        .navigationBarHidden(true)
+        
+        ZStack{
+          Rectangle()
+            .fill(Color.white)
+            .frame(height:84)
+            .cornerRadius(10)
+            .padding(.leading, 16)
+            .padding(.trailing, 16)
+          HStack{
+            Image("icon_magnifyingGlass")
+              .padding(.leading, 36)
+            VStack(spacing: 4){
+              Text("망고에게 치석이 있는 것 같나요?")
+                .font(.system(size: 10))
+                .foregroundColor(Color(hex: "50505E"))
+                .padding(.leading, 6)
+                .frame(maxWidth: .infinity, alignment: .leading)
+              Text("치석이 의심되는 부분을 AI로 판단할 수 있어요")
+                .font(.system(size: 12))
+                .foregroundColor(Color(hex: "0062FF"))
+                .padding(.leading, 6)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            Spacer()
+          }
+        }
+        
+        ZStack{
+          Rectangle()
+            .fill(Color.white)
+            .frame(height: 320)
+            .cornerRadius(10)
+            .padding(.leading, 16)
+            .padding(.trailing, 16)
+            .cornerRadius(10)
+          
+          VStack{
+            Text("주의 사항")
+              .fontWeight(.semibold)
+              .font(.system(size: 16))
+              .padding(.top, 24)
+            Text("반려견의 입술을 들어올려 치아가 잘 보이도록 찍어주세요!")
+              .font(.system(size: 12))
+              .padding(.top, 10)
+            HStack(spacing: 22){
+              VStack(spacing: 6){
+                Image("image_tartar2")
+                Text("예시 사진")
+                  .font(.system(size: 10))
+                  .foregroundColor(Color(hex: "50505E"))
+              }
+              VStack(spacing: 6){
+                Image("image_tartar3")
+                Text("예시 사진")
+                  .font(.system(size: 10))
+                  .foregroundColor(Color(hex: "50505E"))
+              }
+            }.padding(.top, 24)
+            
+            Spacer()
+            
+            
+            ImageView(pickedImage: $pickedImage) { selectedImage in
+              tartarAnalysisVM.analyzeImage(selectedImage) { success in
+                if success {
+                  showingDiagnosisView = true
+                } else {
+                  print("분석 실패")
+                }
+              }
+            }
+            Spacer()
+          }.frame(height: 320)
+        } // ZStack
+        
+        
+        Spacer()
+        
+      } // VStack
+      .background(Color(hex: "F5F5F5"))
+      
+      if tartarAnalysisVM.isLoading {
+        ZStack {
+          Color.black
+            .opacity(0.5)
+          
+          VStack {
+            ProgressView()
+              .foregroundStyle(.white)
+              .progressViewStyle(CircularProgressViewStyle(tint: .white))
+              .scaleEffect(3)
+              .padding(20)
+              .shadow(radius: 10)
+            
+            Text("치석 분석 중...")
+              .font(.subheadline)
+              .foregroundColor(.white)
+              .padding(.top, 10)
+          }
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+      }
+    } // ZStack
+    .navigationBarTitle("", displayMode: .inline)
+    .navigationBarHidden(true)
+    .sheet(isPresented: $showingDiagnosisView) {
+      let imageSize = tartarAnalysisVM.imageSize
+      let result = tartarAnalysisVM.analysisResult
+      DiagnosisView(result: result, imageSize: imageSize!, count: result.count, pickedImage: $pickedImage)
     }
-}
-
-struct UploadImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        UploadImageView()
-    }
+  }
 }
