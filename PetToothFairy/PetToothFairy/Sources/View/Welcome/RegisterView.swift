c948c541c608b0ff9a5ee22829b0d5fe99fccd8e
임주민 <limj1213@gmail.com>
2024-12-06 15:12:51 +0900

    feat - #11 로그인 및 회원가입 화면에서 네트워크 통신 뷰모델 함수 호출

--- PetToothFairy/PetToothFairy/Sources/View/Welcome/RegisterView.swift
+++ PetToothFairy/PetToothFairy/Sources/View/Welcome/RegisterView.swift
@@ -8,104 +8,100 @@
 import SwiftUI
 
 struct RegisterView: View {
-    
-    @State private var name: String = ""
-    @State private var kg: Int = 0
-    let numberRange = 1...30
-    
-    var body: some View {
-        ZStack {
-            ZStack{
-                Color(hex: "F5F5F5")
-                    .ignoresSafeArea()
-                
-                VStack {
-                    VStack{
-                        Text("반려견 양치 생활")
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(Color.blue)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 27)
-                            .padding(.top, 36)
-                        
-                        Text("에 더 도움이 될 수 있도록\n몇 가지를 여쭤볼게요.")
-                            .font(.title)
-                            .lineSpacing(10)
-                            .bold()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 27)
-                            .padding(.top, 1)
-                    }
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 30)
-                            .foregroundColor(Color.white)
-                            .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: -2)
-                            .padding(.top, 20)
-                        
-                        VStack{
-                            Text("반려견의 이름을 알려주세요.")
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .bold()
-                                .padding(.leading, 27)
-                                .padding(.top, 80)
-                            
-                            ZStack{
-                                TextField("", text: $name)
-                                    .padding(.leading, 27)
-                                    .padding(.trailing, 90)
-                                Divider()
-                                    .background(Color.black)
-                                    .padding(.leading, 27)
-                                    .padding(.trailing, 90)
-                                    .padding(.top, 43)
-                            }
-                            
-                            Text("반려견의 몸무게를 알려주세요.")
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .bold()
-                                .padding(.leading, 27)
-                                .padding(.top, 60)
-                            Picker("숫자를 선택하세요", selection: $kg) {
-                                ForEach(numberRange, id: \.self) { number in
-                                    Text("\(number) kg").tag(number)
-                                }
-                            }
-                            .pickerStyle(WheelPickerStyle())
-                            .frame(maxHeight: 150, alignment: .top)
-                            
-                            
-                            NavigationLink(destination: ContentView()) {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .foregroundColor(Color(hex: "3561E6"))
-                                        .frame(height: 48)
-                                    Text("작성 완료")
-                                        .bold()
-                                        .foregroundColor(Color.white)
-                                }
-                            }
-                            .padding(.horizontal, 47)
-                            .padding(.top, 50)
-                            
-                            Spacer()
-                        }
-                    }
-                    .ignoresSafeArea()
+  @ObservedObject var registerViewModel: RegisterViewModel
+  @Binding var socialToken: String
+  
+  @State private var name: String = ""
+  @State private var kg: Int = 0
+  let numberRange = 1...30
+  
+  var body: some View {
+    ZStack {
+      ZStack{
+        Color(hex: "F5F5F5")
+          .ignoresSafeArea()
+        
+        VStack {
+          VStack{
+            Text("반려견 양치 생활")
+              .font(.title)
+              .bold()
+              .foregroundColor(Color.blue)
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.leading, 27)
+              .padding(.top, 36)
+            
+            Text("에 더 도움이 될 수 있도록\n몇 가지를 여쭤볼게요.")
+              .font(.title)
+              .lineSpacing(10)
+              .bold()
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.leading, 27)
+              .padding(.top, 1)
+          }
+          
+          ZStack {
+            RoundedRectangle(cornerRadius: 30)
+              .foregroundColor(Color.white)
+              .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: -2)
+              .padding(.top, 20)
+            
+            VStack{
+              Text("반려견의 이름을 알려주세요.")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .bold()
+                .padding(.leading, 27)
+                .padding(.top, 80)
+              
+              ZStack{
+                TextField("", text: $name)
+                  .padding(.leading, 27)
+                  .padding(.trailing, 90)
+                Divider()
+                  .background(Color.black)
+                  .padding(.leading, 27)
+                  .padding(.trailing, 90)
+                  .padding(.top, 43)
+              }
+              
+              Text("반려견의 몸무게를 알려주세요.")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .bold()
+                .padding(.leading, 27)
+                .padding(.top, 60)
+              Picker("숫자를 선택하세요", selection: $kg) {
+                ForEach(numberRange, id: \.self) { number in
+                  Text("\(number) kg").tag(number)
                 }
+              }
+              .pickerStyle(WheelPickerStyle())
+              .frame(maxHeight: 150, alignment: .top)
+              
+              
+              NavigationLink(destination: ContentView()) {
+                ZStack {
+                  RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(Color(hex: "3561E6"))
+                    .frame(height: 48)
+                  Text("작성 완료")
+                    .bold()
+                    .foregroundColor(Color.white)
+                }
+              }
+              .padding(.horizontal, 47)
+              .padding(.top, 50)
+              
+              Spacer()
             }
+          }
+          .ignoresSafeArea()
         }
-        .padding(.top, 0)
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitle("", displayMode: .inline)
-        .navigationBarHidden(true) 
-        .background(Color.clear)
+      }
     }
-}
-
-struct RegisterView_Previews: PreviewProvider {
-    static var previews: some View {
-        RegisterView()
-    }
+    .padding(.top, 0)
+    .navigationBarBackButtonHidden(true)
+    .navigationBarTitle("", displayMode: .inline)
+    .navigationBarHidden(true)
+    .background(Color.clear)
+  }
 }
