503fe0efe15e73d8b741919209c294825cf4b43c
임주민 <limj1213@gmail.com>
2024-12-06 14:42:21 +0900

    chore - #6 contentView 줄 간격 수정

--- PetToothFairy/PetToothFairy/Sources/View/ContentView.swift
+++ PetToothFairy/PetToothFairy/Sources/View/ContentView.swift
@@ -8,55 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection: Tab = .brushing
-    
-    enum Tab {
-        case brushing
-        case tartar
-        case mypage
+  @State private var selection: Tab = .brushing
+  
+  enum Tab {
+    case brushing
+    case tartar
+    case mypage
+  }
+  
+  init() {
+    UITabBar.appearance().backgroundColor = UIColor(Color(hex: "1B1B23"))
+  }
+  
+  var body: some View {
+    TabView(selection: $selection) {
+      NavigationStack {
+        StartBrushingView()
+      }
+      .tabItem {
+        Image("icon_toothbrush").renderingMode(.template)
+        Text("양치 하기")
+      }
+      .tag(Tab.brushing)
+      
+      NavigationStack {
+        UploadImageView()
+      }
+      .tabItem {
+        Image("icon_diagnosis").renderingMode(.template)
+        Text("치석 판단")
+      }
+      .tag(Tab.tartar)
+      NavigationStack {
+        MyPageView()
+      }
+      .tabItem {
+        Image("icon_mypage").renderingMode(.template)
+        Text("마이페이지")
+      }
+      .tag(Tab.mypage)
     }
-    
-    init() {
-        UITabBar.appearance().backgroundColor = UIColor(Color(hex: "1B1B23"))
-    }
-    
-    var body: some View {
-        TabView(selection: $selection) {
-            NavigationStack {
-                StartBrushingView()
-            }
-            .tabItem {
-                Image("icon_toothbrush").renderingMode(.template)
-                Text("양치 하기")
-            }
-            .tag(Tab.brushing)
-            
-            NavigationStack {
-                UploadImageView()
-            }
-            .tabItem {
-                Image("icon_diagnosis").renderingMode(.template)
-                Text("치석 판단")
-            }
-            .tag(Tab.tartar)
-            NavigationStack {
-                MyPageView()
-            }
-            .tabItem {
-                Image("icon_mypage").renderingMode(.template)
-                Text("마이페이지")
-            }
-            .tag(Tab.mypage)
-        }
-        .tint(.white)
-        .accentColor(Color(hex: "474753"))
-        .navigationBarHidden(true)
-    }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    .tint(.white)
+    .accentColor(Color(hex: "474753"))
+    .navigationBarHidden(true)
+  }
 }
