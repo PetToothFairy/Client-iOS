7c6830d97995b4acc04d136563df9ccc3e934bb1
임주민 <limj1213@gmail.com>
2024-12-09 14:28:58 +0900

    feat - #11 회원가입을 처리하기 위한 ResiterViewModel 구현

--- PetToothFairy/PetToothFairy/Sources/ViewModel/RegisterViewModel.swift
+++ PetToothFairy/PetToothFairy/Sources/ViewModel/RegisterViewModel.swift
@@ -10,13 +10,18 @@ import Combine
 import Alamofire
 
 class RegisterViewModel: ObservableObject {
-  var subscription = Set<AnyCancellable>()
-  
+  @Published var showContent: Bool
   @Published var registerResponse: LoginResponse
   
+  var subscription = Set<AnyCancellable>()
+  
   init() {
-    registerResponse = LoginResponse(status: 0, body: "", message: Message(accessToken: "", refreshToken: ""))
-    print(#fileID, #function, #line, "")
+    registerResponse = LoginResponse(status: 0, message: "", data: TokensResponse(accessToken: "", refreshToken: ""))
+    showContent = false
+  }
+  
+  func toggleRegister(){
+    showContent.toggle()
   }
   
   func register(socialAccessToken: String, petName: String, petWeight: Int) {
@@ -25,10 +30,26 @@ class RegisterViewModel: ObservableObject {
       .value()
       .sink(
         receiveCompletion: { completion in
+          switch completion {
+          case .finished:
+            print("Register Request completed.")
+          case .failure(let error):
+            print("Error: \(error)")
+          }
         },
-        receiveValue: { receivedValue in
-          self.registerResponse = receivedValue
-        })
+        receiveValue: { [weak self] receivedValue in
+          self?.registerResponse = receivedValue
+          if receivedValue.status == 200 {
+            self?.toggleRegister()
+            TokenManager.accessToken = receivedValue.data?.accessToken
+            TokenManager.refreshToken = receivedValue.data?.refreshToken
+            print("👉 Access Token : \(receivedValue.data?.accessToken ?? "No Access Token")")
+            print("👉 Refresh Token : \(receivedValue.data?.refreshToken ?? "No Refresh Token")")
+          } else {
+            print("Failed with status \(receivedValue.status): \(receivedValue.message ?? "No message")")
+          }
+        }
+      )
       .store(in: &subscription)
   }
 }
